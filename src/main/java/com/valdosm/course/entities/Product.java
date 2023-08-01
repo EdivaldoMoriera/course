b923b990b2f875902f3361079ab6a6bcdb4e5455
Moreira <valdosm.moreira@gmail.com>
2023-08-01 04:04:11 -0300

    associação parte 2

--- src/main/java/com/valdosm/course/entities/Product.java
+++ src/main/java/com/valdosm/course/entities/Product.java
@@ -25,8 +25,8 @@ public class Product implements Serializable{
     private String imgUrl;
 
     @ManyToMany
+  
     @JoinTable(name = "tb_product_category", joinColumns =  @JoinColumn(name= "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
-    
     private Set<Category> categories = new HashSet<>();//colecao não pode comecar valendo nula, tem que começar vazia porem instanciada
 
     public Product() {
